3dcb1338ab6343444f74b0dafa68b46cabc9f5ce
Tuva Gyllensten <tuvagyllensten@gmail.com>
2025-04-02 12:06:11 +0200

    added components folders + tsx

---
+++ frontend/src/components/menyval1Affärsplanen/Affärsplanen.tsx
@@ -0,0 +1,9 @@
+const Affärsplan = () => {
+    return(
+        <>
+            <h1>Affärsplan</h1>
+        </>
+    )
+}
+
+export default Affärsplan;
---
+++ frontend/src/components/menyval2Projektidé/projektidé.tsx
@@ -0,0 +1,9 @@
+const Projektidé = () => {
+    return(
+        <>
+            <h1>ProjektIdé</h1>
+        </>
+    )
+}
+
+export default Projektidé;
