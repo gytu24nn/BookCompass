ba4457d51083f8ea3aafbc3c0cd5f34ba76024db
Tuva Gyllensten <tuvagyllensten@gmail.com>
2025-04-13 17:59:34 +0200

    added comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,13 +9,24 @@ function App() {
   return (
     <>
       <div>
+        {/*här använder jag mig av router för att jag ska kunna ha en meny som användaren kan välja mellan.*/}
         <Router>
           <Routes>
+            {/*All routing ligger i layout komponenten den fungerar som en mall med header, main och header.*/}
             <Route path='/' element={<Layout />}>
-              <Route index element={<h2 className="homePageHeader">Welcome to BookCompass</h2>} />
+
+              {/*Startsida: visas när använder går till /*/}
+              <Route index element={<h2 className="homePageHeader">Welcome to BookCompass</h2>} /> 
+
+              {/*sida för affärsplanen visas när man går till /affärsplan (kommer bytas ut senare till något annat passande för bookcompass)*/}
               <Route path='affärsplan' element={<Affärsplan />} />
+
+              {/*Sida för skapa konto visas när man går till /create-account*/}
               <Route path="create-account" element={<CreateAccount />} />
+
+              {/*Sida för att logga in visas när man går till /login*/}
               <Route path="login" element={<LogIn />} />
+              
             </Route>
           </Routes>
         </Router>
