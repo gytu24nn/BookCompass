882a974c9eda43f9eda8f39503f7642c69c123d4
Tuva Gyllensten <tuvagyllensten@gmail.com>
2025-05-08 11:56:51 +0200

    fixed gitignore

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca272625bb87eabfd8a93a86ad966c060529a5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfbb1029a0067f2cb0d698a43dc70bf3fdd91ce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
