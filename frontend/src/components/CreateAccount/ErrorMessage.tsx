1ccf0a944ddb9bb9a7805653e00c6a4dead56ff0
Tuva Gyllensten <tuvagyllensten@gmail.com>
2025-05-26 14:47:28 +0200

    splited createdAccount component to different smal

---
+++ frontend/src/components/CreateAccount/ErrorMessage.tsx
@@ -0,0 +1,5 @@
+const ErrorMessage = ({ message }: { message: string }) => (
+    <p className="errorMessage">{message}</p>
+);
+
+export default ErrorMessage; 
---
+++ frontend/src/components/CreateAccount/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+const LoadingSpinner = () => (
+    <div className="accountCreatedContainer">
+        <div id="spinningWheelLoading"></div>
+        <p>Your account has been created. You will now be sent to the login page...</p>
+    </div>
+)
+
+export default LoadingSpinner;
